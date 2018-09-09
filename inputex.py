45434f07dd319f48195ffceb20957f268e340359
louAphasia <42347224+louAphasia@users.noreply.github.com>
2018-09-09 18:57:14 +0200

    0909 dict txt ++

--- inputex.py
+++ inputex.py
@@ -14,4 +14,30 @@ print("wwww")
 #------------------
 
 print(time.time())
+#----------- joiny exercise
 
+
+list2=['avc','ert','123']
+
+list2='www'.join(list2)
+
+print(list2)
+
+
+'''mess='ali koty456\
+                    pieski'
+
+LETTERS_AND_SPACE='qwertyuiopasdfghjklzxcvbnm'
+
+
+
+def removeNonLetters(mess):
+    lettersOnly=[]
+    for s in mess:
+        if s in LETTERS_AND_SPACE:
+            lettersOnly.append(s)
+    return 'ccc'.join(lettersOnly)
+
+we=removeNonLetters(mess)
+
+print(we)'''
