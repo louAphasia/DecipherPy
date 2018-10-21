055b245d9ce7f502f9fd3b3526dfbe15a0c91249
louAphasia <42347224+louAphasia@users.noreply.github.com>
2018-10-21 15:38:44 +0200

    commit bewest correct

--- RSAkluczpublModulo.py
+++ RSAkluczpublModulo.py
@@ -13,18 +13,17 @@ while t%2==0:
 
 a=2
 while a<100:
-    x
     k=t
     while k<kphi:
-        x=pow(a,k,n)
+        x= pow(a,k,n)
         if x!=1 and x!=(n-1) and pow(x,2,n)==1:
             p=gcd(x-1,n)
             break
         k=k*2
     a=a+2
 
-q=n/p
-assert (p*q)==n
+q=n//p
+assert(p*q)==n
 print('p=',p)
 print('q =',q)
 
